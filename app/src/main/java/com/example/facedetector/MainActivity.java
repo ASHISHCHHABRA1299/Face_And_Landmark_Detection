57e251c2693084f4b28078aa43543837eef5a76f
hp <chhabraashish123@gmail.com>
2020-06-07 03:21:37 +0530

    Rectangle Canvas Updated

--- app/src/main/java/com/example/facedetector/MainActivity.java
+++ app/src/main/java/com/example/facedetector/MainActivity.java
@@ -5,8 +5,13 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.graphics.Rect;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -58,6 +63,9 @@ public class MainActivity extends AppCompatActivity {
             i.setImageURI(data.getData());
             FirebaseVisionImage image;
             try {
+                Bitmap bmp= MediaStore.Images.Media.getBitmap(this.getContentResolver(),data.getData()) ;
+                final Bitmap mutablebmp=bmp.copy(Bitmap.Config.ARGB_8888,true);
+                final Canvas canvas=new Canvas(mutablebmp);
                 image = FirebaseVisionImage.fromFilePath(getApplicationContext(), data.getData());
                 FirebaseVisionFaceDetectorOptions options =
                         new FirebaseVisionFaceDetectorOptions.Builder()
@@ -78,6 +86,11 @@ public class MainActivity extends AppCompatActivity {
                                                 // ...
                                                 for (FirebaseVisionFace face : faces) {
                                                     Rect bounds = face.getBoundingBox();
+                                                    Paint p=new Paint();
+                                                    p.setColor(Color.YELLOW);
+                                                    p.setStyle(Paint.Style.STROKE);
+                                                    canvas.drawRect(bounds,p);
+                                                    i.setImageBitmap(mutablebmp);
                                                     float rotY = face.getHeadEulerAngleY();  // Head is rotated to the right rotY degrees
                                                     float rotZ = face.getHeadEulerAngleZ();  // Head is tilted sideways rotZ degrees
 
